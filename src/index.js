4b8a47c17af3329ad8be9d9c70aa7875d5a7bb64
yjw8860 <botoks@naver.com>
2020-10-11 14:42:44 +0900

    Redux로 플러스 마이너스 구현

--- src/index.js
+++ src/index.js
@@ -1,25 +1,39 @@
+import {createStore} from 'redux'
+
 const add = document.querySelector('#add');
 const minus = document.querySelector('#minus');
 const number = document.querySelector('span');
+const ADD = 'ADD';
+const MINUS = 'MINUS';
 
-let count = 0;
+number.innerText = 0;
 
-number.innerText = count;
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1
+    case MINUS:
+      return count - 1
+    default:
+      return count;
+  }
+};
 
-const updateText = () => {
-  number.innerText = count;
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
 }
 
-const handleAdd = () =>{
-  count = count + 1;
-  updateText();
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({type:ADD});
 }
 
-const handleMinus = () =>{
-  count = count - 1;
-  updateText();
+const handleMinus = () => {
+  countStore.dispatch({type:MINUS});
 }
 
-
-add.addEventListener('click', handleAdd)
-minus.addEventListener('click', handleMinus)
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
